d4c8d9ea898eae5060bd8165627b75dff6b7e17c
ricardoMiranda <mail@ricardoMiranda.com>
2016-04-25 11:43:48 +0000

    Small refactoring

--- Individual.hs
+++ Individual.hs
@@ -4,23 +4,23 @@ import System.Random
 import System.Random.Shuffle
 
 -- A chromosome is a sequense cities to visit (order, name of the city)
-data Chromosome = Chromosome { genes :: [ (Int, Int) ] -- [ (pos, chromosome) ]
-                             , fitness :: Maybe Int } -- total distance 
+data Individual = Individual { chromosome :: [ (Int, Int) ] -- [ (pos, gene) ]
+                             , fitness :: Maybe Float } -- total distance 
 	            deriving Show
 
-instance Eq Chromosome where
-  Chromosome { fitness = a } == Chromosome { fitness = b } = Just a == Just b
+instance Eq Individual where
+  Individual { fitness = a } == Individual { fitness = b } = Just a == Just b
 
-instance Ord Chromosome where
-  compare Chromosome { fitness = a } Chromosome { fitness = b } = compare (Just a) (Just b)
+instance Ord Individual where
+  compare Individual { fitness = a } Individual { fitness = b } = compare (Just a) (Just b)
 
-newChromosome :: [ (Int, Int) ] -> Maybe Int -> Chromosome
-newChromosome gs f = Chromosome { genes = gs
+newIndividual :: [ (Int, Int) ] -> Maybe Float -> Individual
+newIndividual gs f = Individual { chromosome = gs
                                 , fitness = f }
 
-createChromosome :: (RandomGen g) => g -> Int -> (Chromosome, g)
-createChromosome g n =
+createIndividual :: (RandomGen g) => g -> Int -> (Individual, g)
+createIndividual g n =
   let xs' = [ 1..n ] in
   let xs = zip [ 1.. ] (shuffle' xs' (length xs) g) in
   let (_, g') = next g in
-  (newChromosome xs Nothing, g')
+  (newIndividual xs Nothing, g')
